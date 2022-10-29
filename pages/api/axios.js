cd7a60ff8c728b47eeec101ded2e6e1ae0d18163
Lamiaa Gebril <lamiaamonaem6@gmail.com>
2022-10-30 00:20:05 +0200

    version 1.2.0

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 if(typeof window !== "undefined") {
     axios.defaults.baseURL = `${window.location.origin}/api`;
-} else axios.defaults.baseURL = 'https://basic-travel-blog.vercel.app/api';
+} else axios.defaults.baseURL = 'basic-travel-blog-5t26z1ud4-l-gebril.vercel.app/api';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';

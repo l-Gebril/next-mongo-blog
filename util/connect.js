65875a21224a338ffd28e2cb2c241476e78d6bcf
Lamiaa Gebril <lamiaamonaem6@gmail.com>
2022-10-29 21:58:55 +0200

    Untracked files issue resolved to fix .gitignore

--- util/connect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export default async function connect() {
-    mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log("Database Connected Successfully!"))
-    .catch(() => console.log("Database Can't Connect!"))
-}

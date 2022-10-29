9129a6bb133ec5ffae179ba9631c5530981f3a90
Lamiaa Gebril <lamiaamonaem6@gmail.com>
2022-10-29 21:59:17 +0200

    version 1.2.0

---
+++ components/Layout.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+
+const Header = () => {
+    return(
+        <header className="text-sm md:text-base flex items-center border border-gray-300">
+            <nav className="container flex justify-between items-center gap-2">
+            <h1><Link href="/" className="font-bold text-lg md:text-2xl">Travel Blog</Link></h1>
+            <ul className="flex justify-between items-center gap-2">
+                <li><Link className="hover:underline hover:text-main py-1 px-3 tranistion-all duration-300" href="/">Home</Link></li>
+                <li><Link href="/create/article" className="bg-main text-white py-1 px-3 rounded border hover:border-main hover:bg-white hover:text-slate-800 tranistion-all duration-300">Create Article</Link></li>         
+            </ul>
+            </nav>
+        </header>
+    )
+}
+
+const Footer = () => {
+    return <footer className="h-[60px] flex justify-center items-center border-t border-gray-300">Coded & Developed By <Link href="/" className="hover:underline hover:text-main py-1 px-3 tranistion-all duration-300">Lamiaa Gabriel</Link> </footer>
+}
+const Layout = ({ children }) => {
+    return (
+        <>
+        <Header />
+        
+        <main className="py-6 bg-gray-50">
+            <div className="container grow">{ children }</div>
+        </main>
+        <Footer />
+        </>
+    );
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/legacy/image";
+
+export default function Home({ articles }) {
+  return(
+    <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+        {
+          articles.length? (articles.map((article, i) => (
+            <article key={i} className="relative bg-white text-black shadow-md rounded overflow-hidden">
+              <Link href={`/article/${article._id}`}>
+                  <Image
+                  src={article.poster}
+                  alt={`${article.title} poster`}
+                  width={16}
+                  height={9}
+                  layout='responsive'
+                  objectFit="cover"
+                  objectPosition='center'
+                  />
+                <main className="container pt-3 text-sm">
+                  <h1 className="text-base font-bold hover:underline hover:text-main transition-all duration-300 line-clamp-2 mb-2">{article.title}</h1>
+                  <p className="line-clamp-4">{article.topic}</p>
+                  <button></button>
+                </main>
+              </Link>
+            </article>
+          ))): <h1>No Posts Yet</h1>
+        }
+      </div>
+    );
+}
+
+export async function getStaticProps() {
+  const result = await fetch('http://localhost:3000/api/article', { method: 'GET' }).then(res => res.json());
+  return {
+    props: { articles: result.docs? result.docs : [] },
+    revalidate: 10
+  }
+}
